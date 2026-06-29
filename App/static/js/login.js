a4045a3b06bf34c057284c99b87e7825469cf2cf
MatiasHayes <hayes.matias.et21.22@gmail.com>
2026-06-29 10:35:32 -0300

    Agregamos los primeros archivos

---
+++ App/static/js/login.js
@@ -0,0 +1,17 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch("/login", {
+        method: "POST",
+        body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+        alert(data.mensaje);
+        if (data.ok) {
+            window.location.href = "/home";
+        }
+    });
+});
---
+++ App/static/js/registro.js
@@ -0,0 +1,17 @@
+document.getElementById("registroForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch("/registro", {
+        method: "POST",
+        body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+        alert(data.mensaje);
+        if (data.ok) {
+            window.location.href = "/login";
+        }
+    });
+});
